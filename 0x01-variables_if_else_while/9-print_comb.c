4637a8df4cea0e2df39c623b7f9c80ed06757fae
Dawitt-dev <davoza27@yahoo.com>
2023-09-08 18:58:47 -0700

    ed_1_tsk_9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,14 @@ int main(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+	for (i = 0; i < 9; i++)
 	{
-		putchar(i);
 		putchar(',');
 		putchar(' ');
 	}
 
-	putchar('\n');
+
 
 	return (0);
 }
