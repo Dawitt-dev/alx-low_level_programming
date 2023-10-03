27c9d0e092ff28fcfd601bd062b6b59be9878e6f
Dawitt-dev <davoza27@yahoo.com>
2023-10-02 19:29:14 -0700

    tsk_3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ *@argc: argument count
+ *@argv: argument vector
+ * Return: always 1
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int z;
+	
+        if (argc < 3)
+        {
+                printf("Error\n");
+                return (1);
+        }
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		z = x * y;
+		printf("%d\n", z);
+	}
+	return (0);
+}
