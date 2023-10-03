cced9379735c420a9a4dd00c4cd9dd8d0e1cf9fe
Dawitt-dev <davoza27@yahoo.com>
2023-10-02 18:42:07 -0700

    tsk_1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it
+ *@argc: argument count
+ *@argv: argument vector
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = argc - 1;
+	(void)argv;
+
+	printf("%d\n", i);
+
+	return (0);
+}
