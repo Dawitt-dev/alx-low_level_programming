84f1a7f1792520f9e79a00650884c08555b8c4e2
Dawitt-dev <davoza27@yahoo.com>
2023-10-08 15:24:57 -0700

    ed_0_tsk_0

--- 0x0A-argc_argv/0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0A-argc_argv/0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,11 +10,15 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *ptr;
 
+	ptr = malloc(b);
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
-	return (ptr);
+	else
+	{
+		return (ptr);
+	}
 }
