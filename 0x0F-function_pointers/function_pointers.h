7135660bb50e42149f998ee4d363d4a8a4068770
Dawitt-dev <davoza27@yahoo.com>
2023-10-11 15:16:37 -0700

    ed_1_whats my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 int int_index(int *array, int size, int (*cmp)(int));
