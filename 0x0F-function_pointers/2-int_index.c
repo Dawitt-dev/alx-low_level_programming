d7ae4b92ba237a14edb56e0d736eeca923dbff82
Dawitt-dev <davoza27@yahoo.com>
2023-10-11 17:26:29 -0700

    #To hell with circumstances I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ *@size: number of elements in the array
+ *@cmp: pointer to the function
+ *@array: pointer to the array
+ * Return: 0,-1 or index of the first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+		{
+			return (i);	
+		}
+	}
+	return (-1);
+}
